d6b63fd57cebccec4e5c3be5d76ffe7e5e87db89
Matthew Steinkamp <mdsteinkamp@gmail.com>
2022-12-20 19:53:31 -0800

    commit tree fc5a48f38399ff8cd5ca00e7c310caced2f9bc6b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(object, event) {
+    const newArray = [];
+    for (let i = 0; i < object.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+/* console.log(writeCards(names, 'surprise'));
+ */
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+    console.log(count--);
+    }
+}
+
+/* console.log(countDown()); */
